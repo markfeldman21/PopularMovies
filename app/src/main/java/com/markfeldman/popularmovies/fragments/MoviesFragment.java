f0af84dcf95401ba31ff958f07983c34b4b2fa41
Mark Feldman <markfeldman21@gmail.com>
2017-01-16 20:18:38 -0500

    Add Progress Bar to Within Loader

--- app/src/main/java/com/markfeldman/popularmovies/fragments/MoviesFragment.java
+++ app/src/main/java/com/markfeldman/popularmovies/fragments/MoviesFragment.java
@@ -1,11 +1,8 @@
 package com.markfeldman.popularmovies.fragments;
 
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -14,13 +11,11 @@ import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.markfeldman.popularmovies.R;
 import com.markfeldman.popularmovies.activities.DetailActivity;
 import com.markfeldman.popularmovies.data_helpers.JSONParser;
@@ -28,11 +23,7 @@ import com.markfeldman.popularmovies.data_helpers.MovieRecyclerAdapter;
 import com.markfeldman.popularmovies.data_helpers.NetworkUtils;
 import com.markfeldman.popularmovies.objects.MovieObj;
 import org.json.JSONException;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -41,7 +32,6 @@ public class MoviesFragment extends Fragment implements MovieRecyclerAdapter.Mov
     private RecyclerView recyclerView;
     private MovieRecyclerAdapter movieRecyclerAdapter;
     private final String INTENT_EXTRA = "Intent Extra";
-    private ProgressDialog progressDialog;
     private TextView errorMessage;
     private final static int SEARCH_LOADER = 22;
     private static final String SEARCH_QUERY_URL_EXTRA = "query";
@@ -66,7 +56,7 @@ public class MoviesFragment extends Fragment implements MovieRecyclerAdapter.Mov
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_movies, container, false);
         errorMessage = (TextView) view.findViewById(R.id.tv_error_message_display);
-
+        progressBar = (ProgressBar)view.findViewById(R.id.pb_loading_indicator);
         recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),2);
 
@@ -95,7 +85,6 @@ public class MoviesFragment extends Fragment implements MovieRecyclerAdapter.Mov
         } else {
             loaderManager.restartLoader(SEARCH_LOADER, queryBundle, this);
         }
-        //new RetrieveMovieInfo().execute();
     }
 
     @Override
@@ -121,7 +110,7 @@ public class MoviesFragment extends Fragment implements MovieRecyclerAdapter.Mov
             String mJsonResult;
             @Override
             protected void onStartLoading() {
-                //progressBar.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
                 super.onStartLoading();
                 if (args == null){
                     return;
@@ -161,7 +150,7 @@ public class MoviesFragment extends Fragment implements MovieRecyclerAdapter.Mov
     @Override
     public void onLoadFinished(Loader<String> loader, String data) {
         MovieObj movies[] = null;
-        //progressBar.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
         if (data == null){
             showErrorMessage();
         }else{
