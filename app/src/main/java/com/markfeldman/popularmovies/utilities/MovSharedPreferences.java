f9012cec77f520965d56cad202e1343124ab006d
Mark Feldman <markfeldman21@gmail.com>
2017-01-19 13:43:54 -0500

    Add Preference Utility Class

---
+++ app/src/main/java/com/markfeldman/popularmovies/utilities/MovSharedPreferences.java
@@ -0,0 +1,17 @@
+package com.markfeldman.popularmovies.utilities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import com.markfeldman.popularmovies.R;
+
+public class MovSharedPreferences {
+    public static String getPreferredWeatherLocation(Context context) {
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(context);
+        String keyForLocation = context.getString(R.string.movie_pref_key);
+        String defaultLocation = context.getString(R.string.pref_default_unit);
+        return prefs.getString(keyForLocation, defaultLocation);
+    }
+}
